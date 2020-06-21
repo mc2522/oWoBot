24ac4b54cca92bab220c55366b459ab8be55fede
Mike Cao <mc2522@rit.edu>
2020-06-21 17:45:17 -0400

    combined logic in switch statement

--- oWoBot.js
+++ oWoBot.js
@@ -13,55 +13,56 @@ client.on('ready', () => {console.log("oWoBot is listening...")})
 
 // when the client gets a message, respond accordingly
 client.on('message', async msg => {
+    // if receiving message from bot, don't do anything 
+    if (msg.author.bot) return
 
-    // check if the command is to play music
-    if (/!play \w+/i.test(msg.content)) {
-        const args = msg.content.split(' ')
+    // check commands/messages
+    switch (true) {
+        // play music from a link
+        case /!play\s*\w*/i.test(msg.content):
+            msg.reply('playing music')
+            const args = msg.content.split(' ')
+            if (args.length != 2) {
+                msg.reply('Invalid Link. Play music command : !play [link]')
+            }
 
-        const play = (connection, msg) => {
+            const play = (connection, msg) => {
+                var server = servers[msg.guild.id]
+                server.dispatcher = connection.playStream(ytdl(server.queue[0], { filter: 'audioonly' }))
+                server.queue.shift()
+                server.dispatcher.on('end', () => {
+                    if (server.queue[0]) {
+                        play(connection, msg)
+                    } else {
+                        connection.disconnect()
+                    }
+                })
+            }
+            
+            const channel = client.channels.cache.get(process.env.MUSIC_ID)
+            // check for channel
+            if (!channel) msg.channel.send('Error: cannot locate music voice channel')
+            // join the channel
+            const connection = await channel.join().then(connection => {
+                const dispatch = connection.play(ytdl(args[1], { filter: 'audioonly' }))
+            }).catch(err => {
+                console.error(err)
+            })
+            // if no queue in server in msg's server (guild), create new queue for server and store it in servers object
+            /*if (!server[msg.guild.id])
+                servers[msg.guild.id] = { queue: [] }
             var server = servers[msg.guild.id]
-            server.dispatcher = connection.playStream(ytdl(server.queue[0], { filter: 'audioonly' }))
-            server.queue.shift()
-            server.dispatcher.on('end', () => {
-                if (server.queue[0]) {
-                    play(connection, msg)
-                } else {
-                    connection.disconnect()
-                }
-            })
-        }
-        
-        const channel = client.channels.cache.get(process.env.MUSIC_ID)
-        // check for channel
-        if (!channel) msg.channel.send('Error: cannot locate music voice channel')
-        // join the channel
-        const connection = await channel.join().then(connection => {
-            const dispatch = connection.play('music.mp3')
+            server.push(ars[1])
+            play(connection, msg)*/
 
-            dispatch.on('finish', () => {
-                msg.reply('Music finished.')
-            })
-        }).catch(err => {
-            console.error(err)
-        })
-        // if no queue in server in msg's server (guild), create new queue for server and store it in servers object
-        /*if (!server[msg.guild.id])
-            servers[msg.guild.id] = { queue: [] }
-        var server = servers[msg.guild.id]
-        server.push(ars[1])
-        play(connection, msg)*/
-
-
-        return
-    }
-
-    // check other commands/messages
-    switch (msg.content.toLowerCase()) {
-        case '!skip':
+            break
+        // skip current music and move on to next in queue WIP
+        case /!skip/i.test(msg.content):
             var server = servers[msg.guild.id]
             if (server.dispatcher) server.dispatcher.end()
-            return
-        case '!stop':
+            break
+        // stop current music WIP
+        case /!stop/i.test(msg.content):
             var server = servers[msg.guild.id]
             if (msg.guild.voiceConnection) {
                 for (let i = server.queue.length - 1; i >= 0; i--) {
@@ -72,24 +73,25 @@ client.on('message', async msg => {
             }
             if (msg.guild.connection) msg.guild.voiceConnection.disconnect()
             break
-        case 'ping':
+        // ping
+        case /ping/i.test(msg.content):
             msg.channel.send('Pong')
-            return
-        case 'knock knock':
+            break
+        // knock knock joke
+        case /knock knock/i.test(msg.content):
             msg.channel.send('Who\'s there?')
-            return
+            break
         // reply with avatar pic
-        case '!avatar':
+        case /!avatar/i.test(msg.content):
             msg.reply(msg.author.displayAvatarURL())
-            return
+            break
+        // if the message starts with !, then the command is not recognized since the previous conditionals didn't pass
+        case /^!/.test(msg.content):
+            msg.reply('Command not recognized')
+            break
     }
-    
-    // if the message starts with !, then the command is not recognized since the previous conditionals didn't pass
-    if (msg.content.substring(0, 1) === '!') 
-        msg.reply('Command not recognized')
 
     return
-
 })
 
 // server greeting for new members
